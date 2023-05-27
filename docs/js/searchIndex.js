07ea8f9bce10ef7440ae5d8922dea219fcc7c9dc
PatriciaGodoyA <100531190+PatriciaGodoyA@users.noreply.github.com>
2023-05-27 19:09:10 +0200

    Add files via upload

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020que\u0020suma\u0020dos\u0020n\u00FAmeros,\u0020que\u0020se\u0020introducen\u0020como\u0020par\u00E1metros",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\esPar\u0028\u0029",
+            "name": "esPar",
+            "summary": "Funci\u00F3n\u0020que\u0020verifica\u0020si\u0020el\u0020n\u00FAmero\u0020introducido\u0020es\u0020par",
+            "url": "namespaces/default.html#function_esPar"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020que\u0020suma\u0020dos\u0020n\u00FAmeros,\u0020que\u0020se\u0020introducen\u0020como\u0020par\u00E1metros",
+            "url": "classes/Calculadora.html#method_sumar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Amultiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "Funci\u00F3n\u0020que\u0020multiplica\u0020los\u0020dos\u0020n\u00FAmeros\u0020introducidos\u0020como\u0020par\u00E1metros",
+            "url": "classes/Calculadora.html#method_multiplicar"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
